fc4d579892bd19b01ead756b81f302437cbe306c
Ethan R Hudson <42320740+Artalius@users.noreply.github.com>
2024-03-18 17:04:41 -0400

    Update lock.cmd
    .

--- Find_Win-Lock-File/lock.cmd
+++ Find_Win-Lock-File/lock.cmd
@@ -7,5 +7,5 @@ for %%I in ("%~dp0.") do set "scriptDirectory=%%~fI"
 REM Assuming "lock.ps1" is located in the same directory as the executing batch file
 set "scriptPath=%scriptDirectory%\lock.ps1"
 
-REM Execute the script with the updated path
-powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; %scriptPath%"
+REM Execute the script with the updated path and pass %%1 input
+powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; %scriptPath% %1"
