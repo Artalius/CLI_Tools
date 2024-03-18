f529e2c8c6f093db6af09a098d465fa023bfb436
Ethan R Hudson <42320740+Artalius@users.noreply.github.com>
2024-03-18 17:02:23 -0400

    Update lock.cmd
    Add Execute PATH handle

--- Find_Win-Lock-File/lock.cmd
+++ Find_Win-Lock-File/lock.cmd
@@ -1,1 +1,11 @@
-powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; C:\lock.ps1"
+@echo off
+setlocal
+
+REM Get the directory of the executing batch file
+for %%I in ("%~dp0.") do set "scriptDirectory=%%~fI"
+
+REM Assuming "lock.ps1" is located in the same directory as the executing batch file
+set "scriptPath=%scriptDirectory%\lock.ps1"
+
+REM Execute the script with the updated path
+powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; %scriptPath%"
